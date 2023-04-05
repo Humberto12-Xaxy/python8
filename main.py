fdfce78ab9bbddc86c5890238ffdd9723e215ad1
Humberto Suriano Medina <humbertosuriano24@gmail.com>
2023-04-05 10:35:13 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import operaciones
+
+if __name__ == '__main__':
+
+    print(operaciones.suma(4, 5))
+    print(operaciones.resta(4, 5))
+    print(operaciones.division(4, 5))
+    print(operaciones.multipicacion(4, 5))
